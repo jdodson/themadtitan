dbe2fe1d2635186b782a4707952d49c557b46230
jdodson <jbdodson@gmail.com>
2019-04-24 20:36:01 -0700

    First version upload.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # themadtitan
-A a script that implements Thanos terrible will.
+A script that implements Thanos terrible will.
+
+[by Jon Dodson](http://jdodson.org)
+
+![alt text](https://i.imgur.com/tYTUwRw.gif "Thanos Snap Animated GIF")
+
+To prepare myself for Avengers Endgame, and to have some fun I decided to write a script
+takes a list of people and at the snap of Thanos fingers, turns half of them to dust.  Because
+that's some crazy mad terrible code (to kill half of anyone in a list) it's written in Perl.  
+I don't mean any offense to Perl, I just thought it's a funny joke.
+
+Feel free to edit the script, add your friends to the list and see who lives and who is
+left to AVENGE the fallen!  I know a zillion people did this in some web app somewhere but
+I wanted this evil to be written in Perl.
+
+Not a Perl expert, so if you have patches to make this better, feel free to submit.
+
+# how this works
+
+Will work out of the box on any Mac and Ubuntu machine in the terminal:
+
+```
+perl the_mad_titan.pl
+```
---
+++ the_mad_titan.pl
@@ -0,0 +1,59 @@
+#
+# the_mad_titan.pl by Jon Dodson (http://jdodson.org)
+#
+# Written in PERL because all crazy shit should be.  Also, not a Perl expert so,
+# you know... chill.
+#
+# MIT License
+#
+# 4/24/2019
+#
+use strict;
+use List::Util qw(shuffle);
+use POSIX;
+
+sub print_people_formatted {
+   my (@people) = @_;
+
+   print "[ ";
+
+   foreach (@people) {
+     print "\"$_\" ";
+   }
+   print " ]\n";
+}
+
+my @people = ("Scarlet Witch", "Black Panther", "Vision", "Captain America", "Black Widow", "Bruce Banner", "Iron Man", "Doctor Strange", "Starlord", "Mantis", "Drax", "Rocket Racoon", "Groot", "Gamora", "Thor", "Spider-Man", "War Machine", "The Winter Soldier");
+
+my @random = shuffle(@people);
+
+my $half = @random.length;
+
+# Thanos rounds up... YOU BASTARD!
+$half = ceil($half / 2);
+
+my @dusted = ();
+my @avengers = ();
+
+for (my $i=0; $i <= @random.length; $i++) {
+   if($i < $half) {
+     push(@dusted, @random[$i]);
+   } else {
+     push(@avengers, @random[$i]);
+   }
+}
+
+# strip whitespace elements https://www.perlmonks.org/?node_id=124970
+@avengers = grep /\S/, @avengers;
+
+print "Thanos: \"It was. And it was beautiful. Titan was like most planets. Too many mouths, not enough to go around. And when we faced extinction, I offered a solution.  At random. Dispassionate, fair. The rich and poor alike. And they called me a madman. And what I predicted, came to pass.\"\n\n";
+print "THE MAD TITAN SNAPS HIS FINGERS!\n";
+print "\n";
+
+print "The people who become dust: ";
+print_people_formatted(@dusted);
+
+print "\n";
+
+print "Avengers Assemble! ";
+print_people_formatted(@avengers);
